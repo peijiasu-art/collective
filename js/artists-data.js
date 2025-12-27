4fdb84f7748fd919c05075b1f1f92eeb9d035cf7
peijiasu-art <peijiasu363@gmail.com>
2025-12-27 14:11:43 +0800

    Create artists-data.js

---
+++ js/artists-data.js
@@ -0,0 +1,30 @@
+// 你只需要改這個檔案就能新增/修改藝術家
+// image 路徑建議：images/artists/xxx.jpg
+// slug 會用來產生網址：artist.html?slug=xxx
+
+window.ARTISTS = [
+  {
+    slug: "a",
+    name: "姓名 A",
+    role: "影像／裝置",
+    keywords: ["記憶", "身體", "低語"],
+    bio: "以記憶與身體感為核心，透過影像與裝置建立觀看的停頓，讓細小情緒成為可被看見的痕跡。",
+    image: "images/artists/artist-a.jpg"
+  },
+  {
+    slug: "b",
+    name: "姓名 B",
+    role: "平面／插畫",
+    keywords: ["城市縫隙", "日常物件"],
+    bio: "關注日常物件被忽略的形狀與位置，以插畫與平面構成重新排列城市的縫隙與生活節奏。",
+    image: "images/artists/artist-b.jpg"
+  },
+  {
+    slug: "c",
+    name: "姓名 C",
+    role: "新媒體",
+    keywords: ["互動", "聲音", "凝視"],
+    bio: "以互動與聲音觸發觀者參與，探討凝視如何被建構、被回望，並在回應中改寫敘事。",
+    image: "images/artists/artist-c.jpg"
+  }
+];
