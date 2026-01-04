f2fb82a59521e33d628b8cdfb97d2b62f90c69d0
peijiasu-art <peijiasu363@gmail.com>
2026-01-04 15:01:53 +0800

    Update artists-data.js

--- js/artists-data.js
+++ js/artists-data.js
@@ -5,26 +5,66 @@
 window.ARTISTS = [
   {
     slug: "a",
-    name: "姓名 A",
-    role: "影像／裝置",
+    name: "夏陽",
+    role: "1932－2022",
     keywords: ["記憶", "身體", "低語"],
     bio: "以記憶與身體感為核心，透過影像與裝置建立觀看的停頓，讓細小情緒成為可被看見的痕跡。",
-    image: "images/artists/artist-a.jpg"
+    image: "images/artists/夏陽.jpg"
   },
   {
     slug: "b",
-    name: "姓名 B",
-    role: "平面／插畫",
+    name: "Anthony Gormley（安東尼・葛姆雷）",
+    role: "1950－",
     keywords: ["城市縫隙", "日常物件"],
     bio: "關注日常物件被忽略的形狀與位置，以插畫與平面構成重新排列城市的縫隙與生活節奏。",
-    image: "images/artists/artist-b.jpg"
+    image: "images/artists/Anthony-Gormley.jpg"
   },
-  {
+    {
     slug: "c",
-    name: "姓名 C",
-    role: "新媒體",
-    keywords: ["互動", "聲音", "凝視"],
-    bio: "以互動與聲音觸發觀者參與，探討凝視如何被建構、被回望，並在回應中改寫敘事。",
-    image: "images/artists/artist-c.jpg"
+    name: "林永政（3cm）",
+    role: "??",
+    keywords: ["城市縫隙", "日常物件"],
+    bio: "關注日常物件被忽略的形狀與位置，以插畫與平面構成重新排列城市的縫隙與生活節奏。",
+    image: "images/artists/林永政.jpg"
+  },
+    {
+    slug: "d",
+    name: "王尋",
+    role: "1962-",
+    keywords: ["城市縫隙", "日常物件"],
+    bio: "關注日常物件被忽略的形狀與位置，以插畫與平面構成重新排列城市的縫隙與生活節奏。",
+    image: "images/artists/王尋.jpg"
+  },
+    {
+    slug: "e",
+    name: "Rona Pondick（羅娜・龐迪克）",
+    role: "1952－",
+    keywords: ["城市縫隙", "日常物件"],
+    bio: "關注日常物件被忽略的形狀與位置，以插畫與平面構成重新排列城市的縫隙與生活節奏。",
+    image: "images/artists/Rona-Pondick.jpg"
+  },
+    {
+    slug: "f",
+    name: "Louise Bourgeois（露易絲・布爾喬亞）",
+    role: "1911–2010",
+    keywords: ["城市縫隙", "日常物件"],
+    bio: "關注日常物件被忽略的形狀與位置，以插畫與平面構成重新排列城市的縫隙與生活節奏。",
+    image: "images/artists/Louise-Bourgeois.jpeg"
+  },
+    {
+    slug: "g",
+    name: "Frederic Fontenoy（弗雷德里克・方特努瓦）",
+    role: "1964－",
+    keywords: ["城市縫隙", "日常物件"],
+    bio: "關注日常物件被忽略的形狀與位置，以插畫與平面構成重新排列城市的縫隙與生活節奏。",
+    image: "images/artists/Frederic-Fontenoy.jpg"
+  },
+    {
+    slug: "h",
+    name: "Henrietta Harris（亨麗埃塔·哈里斯）",
+    role: "1984—",
+    keywords: ["城市縫隙", "日常物件"],
+    bio: "關注日常物件被忽略的形狀與位置，以插畫與平面構成重新排列城市的縫隙與生活節奏。",
+    image: "images/artists/Henrietta-Harris.jpg"
   }
 ];
